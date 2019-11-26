2e3eb248aa64f4193e239d30a53446bd1ac6ab1c
usar <usar@DESKTOP-BMP6C86>
2019-11-26 08:06:11 +0530

    HELLO

--- google/src/main/java/com/google/pojos/student.java
+++ google/src/main/java/com/google/pojos/student.java
@@ -11,5 +11,47 @@ public class student {
 	
 	private String name11;
 	private String name111;
+	public int getNumber() {
+		return number;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName1() {
+		return name1;
+	}
+	public void setName1(String name1) {
+		this.name1 = name1;
+	}
+	public int getStdId() {
+		return stdId;
+	}
+	public void setStdId(int stdId) {
+		this.stdId = stdId;
+	}
+	public String getStdname() {
+		return stdname;
+	}
+	public void setStdname(String stdname) {
+		this.stdname = stdname;
+	}
+	public String getName11() {
+		return name11;
+	}
+	public void setName11(String name11) {
+		this.name11 = name11;
+	}
+	public String getName111() {
+		return name111;
+	}
+	public void setName111(String name111) {
+		this.name111 = name111;
+	}
 
 }
